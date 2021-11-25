3bcf29120b64c4cd6830bbfb18693a1d9c8a8311
Your Name <you@example.com>
2021-11-25 21:38:10 +0300

    add all

---
+++ app/js/main.js
@@ -0,0 +1,61 @@
+
+let swiper = new Swiper(".main-swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+$(function () {
+
+  $(".product-list__btn").on("click", function () {
+    $('.product-list__menu').toggleClass("product-list__menu--active");
+    $('.product-list__btn').toggleClass("product-list__btn--active");
+
+
+  });
+
+  $('.quantity-inner .bt-minus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+  });
+
+  $('.quantity-inner .bt-plus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+  });
+
+  $('.quantity-inner .quantity').bind("change keyup input click", function () {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+      this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+      this.value = parseInt($(this).data('max-count'));
+    }
+  });
+
+
+  var mixer = mixitup('.product-card__wrapper');
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    caption: function (fancybox, carousel, slide) {
+      return (
+        `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
+      );
+    },
+  });
+
+
+});
