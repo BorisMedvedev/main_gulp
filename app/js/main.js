1c2fda113fc971aef75c5b714969919db527f5f3
Your Name <you@example.com>
2021-11-28 18:28:26 +0300

    add bag

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 
+let swiper1 = new Swiper(".myswiper", {
+  slidesPerView: 6,
+  loop: true,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 let swiper = new Swiper(".main-swiper", {
   loop: true,
   pagination: {
@@ -13,16 +24,6 @@ let swiper = new Swiper(".main-swiper", {
 
 
 
-var swiper2 = new Swiper(".mySwiper", {
-  slidesPerView: 6,
-  loop: true,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
 
 
 
@@ -113,11 +114,6 @@ $(function () {
 
 
 
-
-});
-
-$(function () {
-
   $(".star").rateYo({
     numStars: 1,
     ratedFill: "#FFB800",
@@ -125,3 +121,7 @@ $(function () {
   });
 
 });
+
+
+
+
